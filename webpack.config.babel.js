2226ccf71583dd7f79c378caa4d9f711170ca3cd
Theodor Vararu <theo@vararu.org>
2017-01-18 12:37:35 +0000

    Remove unused plugins from webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,35 +1,27 @@
 import webpack from 'webpack'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import autoprefixer from 'autoprefixer'
-import CopyWebpackPlugin from 'copy-webpack-plugin'
 import ReplacePlugin from 'replace-bundle-webpack-plugin'
 import path from 'path'
 import V8LazyParseWebpackPlugin from 'v8-lazy-parse-webpack-plugin'
-import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin'
 const ENV = process.env.NODE_ENV || 'development'
-
-const CSS_MAPS = ENV !== 'production'
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './components/typeahead/wrapper.jsx',
+  entry: './wrapper.jsx',
 
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'accessible-typeahead.min.js'
   },
 
   resolve: {
     extensions: ['', '.jsx', '.js', '.json', '.less'],
     modulesDirectories: [
-      path.resolve(__dirname, 'src/lib'),
       path.resolve(__dirname, 'node_modules'),
       'node_modules'
     ],
     alias: {
-      components: path.resolve(__dirname, 'src/components'),    // used for tests
-      style: path.resolve(__dirname, 'src/style'),
       'react': 'preact-compat',
       'react-dom': 'preact-compat'
     }
@@ -50,35 +42,8 @@ module.exports = {
         loader: 'babel'
       },
       {
-        // Transform our own .(less|css) files with PostCSS and CSS-modules
-        test: /\.(less|css)$/,
-        include: [path.resolve(__dirname, 'src/components')],
-        loader: ExtractTextPlugin.extract('style?singleton', [
-          `css-loader?modules&importLoaders=1&sourceMap=${CSS_MAPS}`,
-          'postcss-loader',
-          `less-loader?sourceMap=${CSS_MAPS}`
-        ].join('!'))
-      },
-      {
-        test: /\.(less|css)$/,
-        exclude: [path.resolve(__dirname, 'src/components')],
-        loader: ExtractTextPlugin.extract('style?singleton', [
-          `css?sourceMap=${CSS_MAPS}`,
-          `postcss`,
-          `less?sourceMap=${CSS_MAPS}`
-        ].join('!'))
-      },
-      {
         test: /\.json$/,
         loader: 'json'
-      },
-      {
-        test: /\.(xml|html|txt|md)$/,
-        loader: 'raw'
-      },
-      {
-        test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
-        loader: ENV === 'production' ? 'file?name=[path][name]_[hash:base64:5].[ext]' : 'url'
       }
     ]
   },
@@ -89,39 +54,28 @@ module.exports = {
 
   plugins: ([
     new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin('style.css', {
-      allChunks: true,
-      disable: ENV !== 'production'
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(ENV)
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'async'
-    }),
-    new CopyWebpackPlugin([
-      { from: './manifest.json', to: './' },
-      { from: './favicon.ico', to: './' }
-    ])
+    })
   ]).concat(ENV === 'production' ? [
     new V8LazyParseWebpackPlugin(),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   output: {
-    //     comments: false
-    //   },
-    //   compress: {
-    //     warnings: false,
-    //     conditionals: true,
-    //     unused: true,
-    //     comparisons: true,
-    //     sequences: true,
-    //     dead_code: true,
-    //     evaluate: true,
-    //     if_return: true,
-    //     join_vars: true,
-    //     negate_iife: false
-    //   }
-    // }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+        negate_iife: false
+      }
+    }),
 
     // strip out babel-helper invariant checks
     new ReplacePlugin([{
