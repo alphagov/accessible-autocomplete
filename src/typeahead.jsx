54c7d1401717ef5122aee57bbf5c876c5abf457e
Theodor Vararu <theo@vararu.org>
2017-02-15 16:24:52 +0000

    Set aria-selected on options when they are focused
    This will make NVDA on IE11 not speak out "not selected" when focusing
    through options.

--- src/typeahead.jsx
+++ src/typeahead.jsx
@@ -224,6 +224,7 @@ export default class Typeahead extends Component {
 
     const Option = ({ children, idx }) =>
       <li
+        aria-selected={selected === idx}
         className={`${cssNamespace}__option`}
         id={`${id}__option--${idx}`}
         onClick={(evt) => this.handleOptionSelect(evt, idx)}
