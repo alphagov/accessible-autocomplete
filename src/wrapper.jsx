e32d9915b3ffb53a8b770b6f2ab64f15d548fa4b
Theodor Vararu <theo@vararu.org>
2017-03-28 14:24:29 +0100

    Add `enhanceSelectElement` function
    This will:
    
    - Place a typeahead input field adjacent to a specified `<select>`
    - Set the typeahead `id` to the `<select>`'s `id`
    - Set the typeahead `name` attribute to '' to prevent form submission
    - Set the typeahead `defaultValue` to the preselected `<option>` if any
    - Set the typeahead `source` to the specified one or to a basic one
      that uses any existing `<option>`s from the `<select>`
    - Hide the `<select>` using inline `display: none`
    - Set the `<select>`'s id to `${id}-select` to decouple from any `<label>`
    - Upon selecting a value in the typeahead, update the original `<select>`

--- src/wrapper.jsx
+++ src/wrapper.jsx
@@ -1,30 +1,88 @@
 import { h, render } from 'preact' /** @jsx h */
 import Typeahead from './typeahead'
 
-if (window) {
-  window.AccessibleTypeahead = function ({
-    autoselect,
-    cssNamespace,
-    defaultValue,
-    displayMenu,
-    element,
-    id,
-    minLength,
-    name,
-    source
-  }) {
-    render(
-      <Typeahead
-        autoselect={autoselect}
-        cssNamespace={cssNamespace}
-        defaultValue={defaultValue}
-        displayMenu={displayMenu}
-        id={id}
-        minLength={minLength}
-        name={name}
-        source={source}
-      />,
-      element
-    )
+function AccessibleTypeahead ({
+  autoselect,
+  cssNamespace,
+  defaultValue,
+  displayMenu,
+  element,
+  id,
+  minLength,
+  name,
+  onSelect,
+  source
+}) {
+  render(
+    <Typeahead
+      autoselect={autoselect}
+      cssNamespace={cssNamespace}
+      defaultValue={defaultValue}
+      displayMenu={displayMenu}
+      id={id}
+      minLength={minLength}
+      name={name}
+      onSelect={onSelect}
+      source={source}
+    />,
+    element
+  )
+}
+
+const createSimpleEngine = results => (query, syncResults) => {
+  const filteredResults = query
+    ? results.filter(r => r.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+    : []
+  syncResults(filteredResults)
+}
+
+AccessibleTypeahead.enhanceSelectElement = ({
+  autoselect,
+  cssNamespace,
+  displayMenu,
+  minLength,
+  selectElement,
+  source
+}) => {
+  if (!source) {
+    const availableOptions = Array.prototype.map.call(selectElement.options, o => o.innerHTML)
+    source = createSimpleEngine(availableOptions)
   }
+
+  const selectedOption = Array.prototype.filter.call(selectElement.options, o => o.selected)[0]
+  const defaultValue = selectedOption ? selectedOption.innerHTML : ''
+
+  const name = ''
+
+  const id = selectElement.id
+
+  const element = document.createElement('span')
+  selectElement.insertAdjacentElement('afterend', element)
+
+  const onSelect = (query) => {
+    const requestedOption = Array.prototype.filter.call(selectElement.options, o => o.innerHTML === query)[0]
+    if (requestedOption) { requestedOption.selected = true }
+  }
+
+  render(
+    <Typeahead
+      autoselect={autoselect}
+      cssNamespace={cssNamespace}
+      defaultValue={defaultValue}
+      displayMenu={displayMenu}
+      id={id}
+      minLength={minLength}
+      name={name}
+      onSelect={onSelect}
+      source={source}
+    />,
+    element
+  )
+
+  selectElement.style.display = 'none'
+  selectElement.id = selectElement.id + '-select'
+}
+
+if (window) {
+  window.AccessibleTypeahead = AccessibleTypeahead
 }
