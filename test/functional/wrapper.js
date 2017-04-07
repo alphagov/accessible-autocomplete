82f511906f52949f66def8304ae64ca3a08aafd5
Nick Colley <nickcolley7@gmail.com>
2017-04-08 00:26:36 +0100

    Check id and name is set when enhancing
    Updates the wrapper.js to make sure name is set

--- test/functional/wrapper.js
+++ test/functional/wrapper.js
@@ -1,9 +1,45 @@
 /* global describe, expect, it */
 import Wrapper from '../../src/wrapper'
 
+const injectSelectToEnhanceIntoDOM = (id) => {
+  const options = {
+    fr: 'France',
+    de: 'Germany',
+    gb: 'United Kingdom'
+  }
+
+  var $select = document.createElement('select')
+  $select.id = id
+  $select.name = id
+
+  for (var optionKey in options) {
+    var option = document.createElement('option')
+    option.value = optionKey
+    option.text = options[optionKey]
+    $select.appendChild(option)
+  }
+
+  document.body.appendChild($select)
+}
+
 describe('Wrapper', () => {
   it('exposes global on window', () => {
     expect(typeof Wrapper).to.equal('object')
     expect(typeof window.AccessibleTypeahead).to.equal('function')
+  })
+  it('can enhance a select element', () => {
+    const id = 'location-picker'
+    injectSelectToEnhanceIntoDOM(id)
+
+    window.AccessibleTypeahead.enhanceSelectElement({
+      selectElement: document.querySelector('#' + id)
+    })
+
+    const typeaheadInstances = document.querySelectorAll('.typeahead__wrapper')
+    expect(typeaheadInstances.length).to.equal(1)
+
+    const typeaheadInstance = typeaheadInstances[0]
+    expect(typeaheadInstance.innerHTML).to.contain(`id="${id}"`)
+    expect(typeaheadInstance.innerHTML).to.contain(`name="${id}"`)
   })
 })
