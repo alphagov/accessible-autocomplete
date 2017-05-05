c0e390f0e38119b8c179a873d80c3c7f85b9c7fb
Theodor Vararu <theo@vararu.org>
2017-05-05 17:55:43 +0100

    Write basic example end to end test

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,11 +1,57 @@
 /* global before, browser, describe, it */
 const expect = require('chai').expect
 
-describe('Accessible Typeahead examples page', () => {
+describe('Accessible Typeahead page', () => {
   before(() => {
     browser.url('/')
   })
+
   it('should have the right title', () => {
     expect(browser.getTitle()).to.equal('Accessible Typeahead examples')
+  })
+
+  describe('basic example', function () {
+    this.retries(3)
+
+    const input = 'input#typeahead-default'
+    const menu = `${input} + ul`
+    const firstOption = `${menu} > li:first-child`
+
+    before(() => {
+      browser.setValue(input, '') // Prevent autofilling, IE likes to do this.
+    })
+
+    it('should show the input', () => {
+      browser.waitForExist(input)
+      expect(browser.isVisible(input)).to.equal(true)
+    })
+
+    it('should allow focusing the input', () => {
+      browser.click(input)
+      expect(browser.hasFocus(input)).to.equal(true)
+    })
+
+    it('should allow typing', () => {
+      browser.keys(['i', 't', 'a'])
+      expect(browser.getValue(input)).to.equal('ita')
+    })
+
+    it('should display suggestions', () => {
+      browser.waitForVisible(menu)
+      expect(browser.isVisible(menu)).to.equal(true)
+    })
+
+    it('should allow selecting an option', () => {
+      browser.keys(['ArrowDown'])
+      expect(browser.hasFocus(input)).to.equal(false)
+      expect(browser.hasFocus(firstOption)).to.equal(true)
+    })
+
+    it('should allow confirming an option', () => {
+      browser.keys(['Enter'])
+      browser.waitUntil(() => browser.getValue(input) !== 'ita')
+      expect(browser.hasFocus(input)).to.equal(true)
+      expect(browser.getValue(input)).to.equal('Italy')
+    })
   })
 })
