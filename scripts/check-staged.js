7e7a6cdc1cfe96383fe86b24fe562ceb9518a8fd
Theodor Vararu <theo@vararu.org>
2017-06-01 14:51:42 +0100

    Update prepush script to only look at file names
    The prepush script will fail because `git diff dist/` returns a really
    long output which causes Node to throw `stdout maxBuffer exceeded`.
    
    `git diff --name-only` outputs only the changed filenames which is
    much shorter.
    
    Also display the actual stack trace in the event of an unexpected
    error.

--- scripts/check-staged.js
+++ scripts/check-staged.js
@@ -1,8 +1,9 @@
 const cp = require('child_process')
 const chalk = require('chalk')
 
-cp.exec('git diff dist/', (err, stdout) => {
+cp.exec('git diff --name-only dist/', (err, stdout) => {
   if (err) {
+    console.log(chalk.red('ERROR:'), err)
     return process.exit(1)
   }
   if (stdout.toString().length) {
