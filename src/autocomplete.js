1543c236836e1e230879dbcc61f7f1f97ef3a110
Sven Tschui <sven.tschui@axa-winterthur.ch>
2017-07-14 13:06:45 +0200

    Introduce the ability to translate the texts and change the dropdown icon

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -54,7 +54,9 @@ export default class Autocomplete extends Component {
     confirmOnBlur: true,
     showNoOptionsFound: true,
     showAllValues: false,
-    required: false
+    required: false,
+    dropdownArrow: null,
+    tNoResults: () => <span>No results found</span>
   }
 
   elementReferences = {}
@@ -384,7 +386,22 @@ export default class Autocomplete extends Component {
   }
 
   render () {
-    const { cssNamespace, displayMenu, id, minLength, name, placeholder, required, showAllValues } = this.props
+    const {
+      cssNamespace,
+      displayMenu,
+      id,
+      minLength,
+      name,
+      placeholder,
+      required,
+      showAllValues,
+      dropdownArrow,
+      tNoResults,
+      tStatusQueryTooShort,
+      tStatusNoResults,
+      tStatusSelectedOption,
+      tStatusResults
+    } = this.props
     const { focused, hovered, menuOpen, options, query, selected } = this.state
     const autoselect = this.hasAutoselect()
 
@@ -427,6 +444,10 @@ export default class Autocomplete extends Component {
           queryLength={query.length}
           minQueryLength={minLength}
           selectedOption={this.templateInputValue(options[selected])}
+          tQueryTooShort={tStatusQueryTooShort}
+          tNoResults={tStatusNoResults}
+          tSelectedOption={tStatusSelectedOption}
+          tResults={tStatusResults}
         />
 
         {showHint && (
@@ -454,7 +475,7 @@ export default class Autocomplete extends Component {
         />
 
         {showAllValues && (
-          <DropdownArrowDown className={dropdownArrowClassName} />
+          dropdownArrow || <DropdownArrowDown className={dropdownArrowClassName} />
         )}
 
         <ul
@@ -488,7 +509,7 @@ export default class Autocomplete extends Component {
           })}
 
           {showNoOptionsFound && (
-            <li className={`${optionClassName} ${optionClassName}--no-results`}>No results found</li>
+            <li className={`${optionClassName} ${optionClassName}--no-results`}>{tNoResults()}</li>
           )}
         </ul>
       </div>
