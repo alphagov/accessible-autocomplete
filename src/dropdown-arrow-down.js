076003f35acbd964636cd20ed297b941b4d340c4
Tobias Gray <tobias.gray@outlook.com>
2017-09-08 11:59:40 +0100

    fix(ie): tab focus fix for IE on SVG element
    When showAllValues is true and the SVG for the dropdown renders IE will see this element as focusable. This fixes this by adding focusable=false so a user does not have to tab twice to move out of the typeahead.

--- src/dropdown-arrow-down.js
+++ src/dropdown-arrow-down.js
@@ -1,7 +1,7 @@
 import { createElement } from 'preact' /** @jsx createElement */
 
 const DropdownArrowDown = ({ className }) => (
-  <svg version='1.1' xmlns='http://www.w3.org/2000/svg' className={className}>
+  <svg version='1.1' xmlns='http://www.w3.org/2000/svg' className={className} focusable='false'>
     <g stroke='none' fill='none' fill-rule='evenodd'>
       <polygon fill='#000000' points='0 0 22 0 11 17' />
     </g>
