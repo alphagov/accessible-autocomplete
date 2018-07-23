461ea0e835ecb9b0577efc9af3b9a18e5a5496a4
Hanna <hannalaakso123@yahoo.co.uk>
2018-07-23 10:27:38 +0100

    Update standard from 10.0.2 to 11.0.1
    Running `npm outdated` in fact only requires
    standard to be updated to 10.0.3 but but this caused tests to fail
    due to static properties being picked up linter as not defined,
    as reported here:
    https://github.com/ricardofbarros/linter-js-standard/issues/113
    
    The main change for 11.0.1 is updating `eslint` to version 4 with
    most changes being to do with indentation rules.
    
    This commit fixes some indentation issues now picked up by linter in
     `src/autocomplete.js`
    
    Tested to work by running all tests and ensuring that the review app
    runs with working examples.

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -26,12 +26,12 @@ function isIosDevice () {
 
 function isPrintableKeyCode (keyCode) {
   return (
-    (keyCode > 47 && keyCode < 58) ||   // number keys
-    keyCode === 32 || keyCode === 8 ||  // spacebar or backspace
-    (keyCode > 64 && keyCode < 91) ||   // letter keys
-    (keyCode > 95 && keyCode < 112) ||  // numpad keys
+    (keyCode > 47 && keyCode < 58) || // number keys
+    keyCode === 32 || keyCode === 8 || // spacebar or backspace
+    (keyCode > 64 && keyCode < 91) || // letter keys
+    (keyCode > 95 && keyCode < 112) || // numpad keys
     (keyCode > 185 && keyCode < 193) || // ;=,-./` (in order)
-    (keyCode > 218 && keyCode < 223)    // [\]' (in order)
+    (keyCode > 218 && keyCode < 223) // [\]' (in order)
   )
 }
 
