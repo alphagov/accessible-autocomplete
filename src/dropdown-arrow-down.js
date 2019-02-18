5acd1b3bfd375c017aa1eef7c87aa457a2c1a130
Dave House <david.house@digital.cabinet-office.gov.uk>
2019-02-18 07:52:32 +0000

    Change size of default icon

--- src/dropdown-arrow-down.js
+++ src/dropdown-arrow-down.js
@@ -3,7 +3,7 @@ import { createElement } from 'preact' /** @jsx createElement */
 const DropdownArrowDown = ({ className }) => (
   <svg version='1.1' xmlns='http://www.w3.org/2000/svg' className={className} focusable='false'>
     <g stroke='none' fill='none' fill-rule='evenodd'>
-      <polygon fill='#000000' points='0 0 22 0 11 17' />
+      <polygon fill='#000000' points='0 0 16 0 8 13' />
     </g>
   </svg>
 )
