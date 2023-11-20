0aef34df65c467d27bb5bdc0bf6849e2563cdbc8
Colin Rotherham <work@colinr.com>
2023-11-20 19:09:11 +0000

    Replace package `uglifyjs-webpack-plugin` with `terser-webpack-plugin`

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
 import webpack from 'webpack'
 import path from 'path'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
-import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 const ENV = process.env.NODE_ENV || 'development'
 
 const plugins = [
@@ -24,18 +24,14 @@ const config = {
 
   optimization: {
     minimize: ENV === 'production',
-    minimizer: [new UglifyJsPlugin({
-      cache: true,
-      parallel: true,
+    minimizer: [new TerserPlugin({
+      extractComments: true,
       sourceMap: true,
-      uglifyOptions: {
-        compress: {
-          negate_iife: false,
-          properties: false
-        },
-        output: {
-          comments: false
-        }
+      terserOptions: {
+        format: { comments: false },
+
+        // Compatibility workarounds
+        safari10: true
       }
     })]
   },
