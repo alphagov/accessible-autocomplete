e3fdcefe092b138a1d4060f29851ca374499c80a
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-09-09 12:09:26 +0100

    correct style rules to use correct hyphenated value

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -573,8 +573,8 @@ export default class Autocomplete extends Component {
             const optionModifierOdd = (index % 2) ? ` ${optionClassName}--odd` : ''
             const iosPosinsetHtml = (isIosDevice())
               ? `<span id=${id}__option-suffix--${index} style="border:0;clip:rect(0 0 0 0);height:1px;` +
-                'marginBottom:-1px;marginRight:-1px;overflow:hidden;padding:0;position:absolute;' +
-                'whiteSpace:nowrap;width:1px">' + ` ${index + 1} of ${options.length}</span>`
+                'margin-bottom:-1px;margin-right:-1px;overflow:hidden;padding:0;position:absolute;' +
+                'white-space:nowrap;width:1px">' + ` ${index + 1} of ${options.length}</span>`
               : ''
 
             return (
